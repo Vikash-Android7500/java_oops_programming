fd90f7ac3a58115f6c0a630c9200e13a27c9e0cf
Your Name <you@example.com>
2022-09-14 13:20:25 +0530

    change inner class

--- Nested_Class/Inner.java
+++
@@ -1,7 +0,0 @@
-package Nested_Class;
-
-public class Inner {
-     void Display(int i){
-          System.out.println("Display: outer_1 Inner Class = " + i);
-     }
-}
--- Nested_Class/Outer.java
+++ Nested_Class/Outer.java
@@ -1,11 +1,19 @@
 package Nested_Class;
 
+//Outer Class
 public class Outer {
      int outer_1 = 100;
 
      void Test() {
           Inner inner = new Inner();
           inner.Display(100);
+     }
+
+     //Inner Class 
+     public class Inner {
+          void Display(int i) {
+               System.out.println("Display: outer_1 Inner Class = " + i);
+          }
      }
 
      public static void CLASS_P2() {
@@ -15,3 +23,4 @@ public class Outer {
      }
 
 }
+
